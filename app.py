2e956ece73d7d64622f1c03ddd1d331977c3ee87
raketyonex <154741306+raketyonex@users.noreply.github.com>
2024-08-21 05:28:48 +0700

    v2

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from bot import chatbot
 
-st.markdown("<h3 style='text-align: center;'>Bzier</h3><h6 style='text-align: center;'>Teman Virtual Kamu</h6>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>TestBot</h3><h6 style='text-align: center;'>customize this text</h6>", unsafe_allow_html=True)
 
 # Inisialisasi session_state untuk menyimpan pesan-pesan chat
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    st.session_state.messages.append({"role": "👩", "content": "kenalin gw Bzier, gw dibuat make LSTM dan ditraining sendiri, bukan pake API openai, jadi sorry kalau jawaban gw aneh ya!"})
+    st.session_state.messages.append({"role": "👩", "content": "Hello guys!"})
 
 # Tampilkan pesan-pesan yang sudah ada di session_state
 for message in st.session_state.messages:
@@ -17,7 +17,7 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.success(message["content"])
 
-prompt = st.chat_input("kirim pesan...")
+prompt = st.chat_input("msg...")
 if prompt:
     bot_response = chatbot(prompt)
 
